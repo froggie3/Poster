8f9c545965d5f28116f7bfc723806ae48d315ffb
Yokkin <yokkin.h@gmail.com>
2023-10-22 12:37:48 +0900

    organized directory

---
+++ src/DataTypes/Weather.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataTypes;
+
+use App\Utils\Telop;
+
+class Weather
+{
+    public $locationUid;
+    public $locationName;
+    public $forecastDate;
+    public $maxTemp;
+    public $maxTempDiff;
+    public $minTemp;
+    public $minTempDiff;
+    public $rainyDay;
+    public $telop;
+    public $weather;
+    public $weatherEmoji;
+
+    public function __construct($uid, $weatherData)
+    {
+        $pref = $weatherData['lv2_info']['name'];
+        $district = $weatherData['name'];
+        $this->locationUid = $uid;
+        $this->locationName = $pref . $district;
+
+        $forecastThreeDays = $weatherData['trf']['forecast'];
+        $forecastToday = $forecastThreeDays[0];
+
+        $this->forecastDate = $forecastToday['forecast_date'];
+        $this->maxTemp = $forecastToday['max_temp'];
+        $this->maxTempDiff = $forecastToday['max_temp_diff'];
+        $this->minTemp = $forecastToday['min_temp'];
+        $this->minTempDiff = $forecastToday['min_temp_diff'];
+        $this->rainyDay = $forecastToday['rainy_day'];
+
+        $this->telop = $forecastToday['telop'];
+        [$this->weather, $this->weatherEmoji] = Telop::TelopData[$this->telop];
+    }
+}
