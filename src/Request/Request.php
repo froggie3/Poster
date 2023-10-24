da32d0d1943cb77b77ad9cb62237290fe9d0ad15
Yokkin <yokkin.h@gmail.com>
2023-10-24 09:50:35 +0900

    add types

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Request;
 
+use Error;
+
 class Request
 {
     protected array $headers;
--- src/app.php
+++ src/app.php
@@ -1,6 +1,8 @@
 #!/usr/bin/env php
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 use App\Request\TenkiAPIRequest;
